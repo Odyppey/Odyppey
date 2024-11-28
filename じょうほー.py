3c3a2e0ff9acec8a8ec6329dc9242e57b5f56f38
Odyppey <23e1810g@shinshu-u.ac.jp>
2024-11-28 17:43:27 +0900

    ふぁーすと

---
+++ じょうほー.py
@@ -0,0 +1,37 @@
+import streamlit as st
+st.title('初めてのstreamlit')
+st.write('これから作品を作っていきます。')
+import streamlit as st
+st.title('初めてのstreamlit')
+st.write('これから作品を作っていきます。')
+st.title('平均体重だみん')
+text = st.text_input('あなたの名前を教えて！')
+st.write('あなたの名前は、' +text+ 'です')
+condition = st.slider('あなたの今の調子は？',0,100,50)
+st.write('コンディション：',condition)
+option = st.selectbox('好きな数字を教えてください',list(['１番','２番','３番','４番']))
+st.write('あなたが選択したのは、'+ option + 'です')
+import time
+st.sidebar.write('プログレスバーの表示')
+
+latest_iteration = st.empty() 
+bar = st.progress(0)
+
+for i in range(100):
+  latest_iteration.text(f'Iteration{i+1}')
+  bar.progress(i +1)
+  time.sleep(0.01)
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+   right_column.write('ここは右カラムです')
+from PIL import Image
+img = Image.open('94644.jpg')
+st.image(img,caption='生活場面',use_column_width=True)
+import pandas as pd
+import numpy as np
+df = pd.DataFrame(
+   np.random.rand(100,2)/[50,50]+[36,64,138,19]
+   columns = ['36.677200485995705', '138.19117963246464',]
+)
+st.map(df)
